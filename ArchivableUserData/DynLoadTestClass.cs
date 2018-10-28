7e6c5c6ca49d994840b4b5d0ae6579408a780c73
Amit Nambiar <amit.lzkpa@gmail.com>
2018-10-28 19:36:02 -0400

    add test class

---
+++ ArchivableUserData/DynLoadTestClass.cs
@@ -0,0 +1,22 @@
+﻿using Rhino;
+
+namespace ArchivableUserData
+{
+    class DynLoadTestClass
+    {
+
+        public string alpha = "alpha";
+        private int beta = 42;
+
+        public void Method1(string gamma)
+        {
+            RhinoApp.WriteLine($"{alpha}-{gamma}");
+        }
+
+        public int Method2(int add)
+        {
+            return beta + add;
+        }
+
+    }
+}
