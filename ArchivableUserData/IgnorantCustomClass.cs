0cd7775598d92f9c3742d99ef35fe1453f01afcd
Amit Nambiar <amit.lzkpa@gmail.com>
2018-10-29 09:11:09 -0400

    add new class to test dynamic deserialization

---
+++ ArchivableUserData/IgnorantCustomClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+namespace ArchivableUserData
+{
+    [Guid("4FDDDFAC-937B-4B1D-A952-08272D84D658")]
+    class IgnorantCustomClass : HotLoadingUserData
+    {
+
+        public override string Description
+        {
+            get { return "Class to test deserialization"; }
+        }
+
+        protected override void OnDuplicate(Rhino.DocObjects.Custom.UserData source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ShouldWrite
+        {
+            get { return true; }
+        }
+
+        protected override bool Read(Rhino.FileIO.BinaryArchiveReader archive)
+        {
+            Rhino.Collections.ArchivableDictionary dict = archive.ReadDictionary();
+            object ho = base.ReadHotLoadData(dict);
+
+            return true;
+        }
+
+        protected override bool Write(Rhino.FileIO.BinaryArchiveWriter archive)
+        {
+            Rhino.Collections.ArchivableDictionary dict = new Rhino.Collections.ArchivableDictionary(1, "CustomData");
+            base.WriteHotLoadData(dict);
+
+            return true;
+        }
+    }
+}
